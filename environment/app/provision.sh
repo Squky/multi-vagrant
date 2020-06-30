81c370ed353c2465c03e859cedd0e6f23bd94568
Squky <jarihussain@hotmail.co.uk>
2020-06-30 12:37:56 +0100

    fixed code so that it actually gets to tests this time

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -11,18 +11,10 @@ echo '----------->> Installing node.js ver6'
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-sudo npm install pm2 -g
 
-echo '----------->> Installing node.js forever?'
 
-cd /node-sample-app
+cd /home/ubuntu/app
 echo 'export DB_HOST=mongodb://192.168.10.150:27017/posts' >> ~/.bashrc
 sudo npm install
-sudo npm start
 
 
-
-
-
-# sudo npm install forever
-# forever start app.js
